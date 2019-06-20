2371c286cbb6bae61a99317160a623716a05148c
Arnbjorn Falmer archer <afalmer-@mi-b3.21-school.ru>
2019-06-20 19:00:20 +0300

    aadd imag

--- views/image/add.php
+++ views/image/add.php
@@ -1,9 +1,9 @@
 <div id="upload">
-    <video id="webcam" autoplay style="transform: scaleX(-1);"></video>
+    <video id="webcam" style="width: 300px; height: 300px;" autoplay style="transform: scaleX(-1);"></video>
     <button id="takePhoto">
         Take a photo
     </button>
-    <canvas id="photo" style="width: 300px; height: 300px;" ></canvas>
+    <canvas id="photo"></canvas>
     <form enctype="multipart/form-data" id="uploadForm" method="POST" >
         <input type="file" name="image" id="userfile" />
         <input type="submit" name="submit" value="Upload" />
@@ -12,14 +12,14 @@
 <script>
     let video = document.getElementById('webcam');
     let canvas = document.getElementById('photo');
+    canvas.width = video.offsetWidth;
+    canvas.height = video.offsetHeight;
     let context = canvas.getContext('2d');
+    let img = new Image();
 
     navigator.getUserMedia({
         audio: false,
-        video: {
-            width: 300,
-            height: 300
-        }
+        video: true
     }, (stream) => {
         video.srcObject = stream;
         video.play();
@@ -31,17 +31,35 @@
         let fr = new FileReader();
 
         fr.onload = () => {
-            let img = new Image();
             img.src = fr.result;
+            context.clearRect(0, 0, canvas.width, canvas.height);
             console.log(img);
-            // context.clearRect(0, 0, canvas.width, canvas.height);
-            context.drawImage(img, 0, 0, 300, 150);
+            context.drawImage(img, 0, 0, 640, 480);
         };
 
         fr.readAsDataURL(userfile.files[0]);
     };
 
     takePhoto.onclick = () => {
-        context.drawImage(video, 0, 0, 300, 150);
+        context.drawImage(video, 0, 0, 300, 300);
     };
+
+    // uploadForm.onsubmit = (e) => { 
+    //     e.preventDefault();
+    //     if (!img.src) {
+    //         return ;
+    //     }
+
+    //     fetch('http://localhost:8080/image/add/', {
+    //         method: 'POST',
+    //         headers: {'Content-type' : 'multipart/form-data'},
+    //         body: new FormData(uploadForm)
+    //     })
+    //         .then((res) => {
+    //             return (res.json());
+    //         })
+    //         .then((data) => {
+    //             alert(data.message);
+    //         });
+    // };
 </script>
